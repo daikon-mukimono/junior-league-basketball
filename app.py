6cd8abd059ef689429fb888e496f81b8d27e6f1a
daikon-mukimono <14227034+daikon-mukimono@users.noreply.github.com>
2019-05-25 22:45:25 -0400

    clean all the data

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ from constants import TEAMS
 from constants import PLAYERS
 
 
-def get_height():
+def clean_height():
 	for player in PLAYERS:
-		player["height"] = player["height"].split(" ")[0]
+		player["height"] = int(player["height"].split(" ")[0])
 
+def clean_exp():
+	for player in PLAYERS:
+		if player["experience"] == "NO":
+			player["experience"] = False
+		else:
+			player["experience"] = True	
 
-
+def clean_guardians():
+	for player in PLAYERS:
+		player["guardians"] = player["guardians"].split(" and ")
+		print(player["guardians"])
 
 if __name__ == "__main__":
-	print(TEAMS)
-	get_height()
+	clean_height()  	# convert height to an int
+	clean_exp()			# convert experience to bool
+	clean_guardians()
+	#print(PLAYERS)
