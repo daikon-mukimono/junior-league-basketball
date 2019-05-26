0c34559d81dcda9264feebdbacd13a4ac7ec2764
daikon-mukimono <14227034+daikon-mukimono@users.noreply.github.com>
2019-05-26 16:02:15 -0400

    decided to hardcode balancing

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from constants import TEAMS
 from constants import PLAYERS
 import copy
 
-
-
 ###### DATA PREP FUNCTIONS ######
 def copy_data():
 	global inner_players
@@ -15,11 +13,18 @@ def clean_height():
 		player["height"] = int(player["height"].split(" ")[0])
 
 def clean_exp():
+	global players_with_experience
+	global players_no_experience
+
+	players_with_experience =[]
+	players_no_experience = []
 	for player in inner_players:
 		if player["experience"] == "NO":
 			player["experience"] = False
+			players_no_experience.append(player)
 		else:
 			player["experience"] = True	
+			players_with_experience.append(player)
 
 def clean_guardians():
 	for player in inner_players:
@@ -34,24 +39,34 @@ def prep_data():
 
 ###### APPLICATION FUNCTIONS ######
 def display_menu():
-	print("Select an option:\n1)BALANCE TEAMS\n2)DISPLAY STATS\n3)QUIT\n")
+	print("Select an option:\n1)DISPLAY STATS\n2)QUIT\n")
 
-def run_application():
-	print("*****WELCOME TO THE KIDS' BASKETBALL LEAGUE TEAM BALANCER*****\n\n")
-	display_menu()
-	exit_loop = 0
+def balance_teams():
+	#this function specially created for three-team, 18-kid league
+	panthers = []
+	bandits = []
+	warriors = []
 
-def balance_team():
-	pass
+	panthers = players_with_experience[:3] + players_no_experience[:3]
+	bandits = players_with_experience[3:6] + players_no_experience[3:6]
+	warriors = players_with_experience[6:9] + players_no_experience[6:9]
+
+def select_team():
 
 def display_stats():
 	pass
+
+def run_application():
+	balance_teams()
+	print("*****WELCOME TO THE KIDS' BASKETBALL LEAGUE TEAM BALANCER*****\n\n")
+	display_menu()
+	exit_loop = 0
 
 	while(exit_loop == 0):
 		try:
 			selection = int(input(">"))
 
-			if selection != 1 and selection != 2 and selection != 3:
+			if selection != 1 and selection != 2:
 				raise ValueError
 
 		except ValueError:
@@ -60,13 +75,11 @@ def display_stats():
 
 		else:
 			exit_loop = 1
-			if selection == 1:
-				balance_team()
 
-			if selection == 2:
+			if selection == 1:
 				display_stats()
 
-			if selection == 3:
+			if selection == 2:
 				print("Goodbye!")	
 
 
