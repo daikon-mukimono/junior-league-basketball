52ec804572d1c84eedacedac683d3a47ae47a19a
daikon-mukimono <14227034+daikon-mukimono@users.noreply.github.com>
2019-05-26 17:17:17 -0400

    ready to submit, but not super elegant or DRY

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def display_menu():
 
 def balance_teams():
 	#this function specially created for three-team, 18-kid league
+	global panthers
+	global bandits
+	global warriors
+
 	panthers = []
 	bandits = []
 	warriors = []
@@ -52,9 +56,89 @@ def balance_teams():
 	warriors = players_with_experience[6:9] + players_no_experience[6:9]
 
 def select_team():
+	print("Select one of the options below!")
+	
+	exit_loop = 0
+	while(exit_loop == 0):
+		try:
+			print("1)Panthers Stats\n2)Bandits Stats\n3)Warriors Stats\n4)Return to main menu\n5)Quit program")
+			selection = int(input(">"))	
 
-def display_stats():
-	pass
+			if selection < 1 or selection > 5:
+				raise ValueError
+		except ValueError:
+			print("try again!")
+		else:
+			exit_loop = 1
+
+			if selection == 1:
+				display_stats(1)
+			elif selection == 2:
+				display_stats(2)
+			elif selection == 3:
+				display_stats(3)
+			elif selection == 4:
+				run_application()
+			elif selection == 5:
+				print("Goodbye!")	
+
+
+def display_stats(team):
+	if team == 1:
+		print("**********************")
+		print("******PANTHERS********")
+		print("**********************\n\n")
+		print("Number of players:" + str(len(panthers)))
+		print("Players:")
+		new_panthers = []
+		for player in panthers:
+			new_panthers.append((player["name"]))
+		print(", ".join(new_panthers))
+		print("Experienced Panthers: " + str(int(len(panthers)/2)))
+		print("Inexperienced Panthers: " + str(int(len(panthers)/2)))
+		total_height = 0
+		for player in panthers:
+			total_height += player["height"]
+		print("Average Height: " + str(round(total_height/len(panthers),2)) + " inches")
+
+	if team == 2:
+		print("**********************")
+		print("*******BANDITS********")
+		print("**********************\n\n")
+		print("Number of players:" + str(len(bandits)))
+		print("Players:")
+		new_bandits = []
+		for player in bandits:
+			new_bandits.append((player["name"]))
+		print(", ".join(new_bandits))
+		print("Experienced Bandits: " + str(int(len(bandits)/2)))
+		print("Inexperienced Bandits: " + str(int(len(bandits)/2)))
+		total_height = 0
+		for player in bandits:
+			total_height += player["height"]
+		print("Average Height: " + str(round(total_height/len(bandits),2)) + " inches")
+
+
+	if team == 3:
+		print("**********************")
+		print("*******WARRIORS*******")
+		print("**********************\n\n")
+		print("Number of players:" + str(len(warriors)))
+		print("Players:")
+		new_warriors = []
+		for player in warriors:
+			new_warriors.append((player["name"]))
+		print(", ".join(new_warriors))
+		print("Experienced Bandits: " + str(int(len(warriors)/2)))
+		print("Inexperienced Bandits: " + str(int(len(warriors)/2)))
+		total_height = 0
+		for player in warriors:
+			total_height += player["height"]
+		print("Average Height: " + str(round(total_height/len(warriors),2)) + " inches")		
+
+	print("\n\nDisplaying team menu:\n")
+	select_team()	
+	
 
 def run_application():
 	balance_teams()
@@ -77,9 +161,9 @@ def run_application():
 			exit_loop = 1
 
 			if selection == 1:
-				display_stats()
+				select_team()
 
-			if selection == 2:
+			elif selection == 2:
 				print("Goodbye!")	
 
 
